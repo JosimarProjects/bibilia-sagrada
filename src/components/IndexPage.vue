c72c03319034775b4239e164e495d2d5dd409002
JosimarProjects <jo030191@gmail.com>
2024-06-01 12:01:53 -0300

    add index

---
+++ src/components/IndexPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <ButtonComponent />
+    <h1>testando</h1>
+  </div>
+</template>
+
+<script>
+import ButtonComponent from '@/components/ButtonComponent.vue'
+
+export default {
+  name: 'IndexPage',
+  components: {
+    ButtonComponent
+  }
+}
+</script>
