44ef125e2f1d6efb83ae1bee1afc3914fe4a0bc9
JosimarProjects <jo030191@gmail.com>
2024-06-01 14:19:20 -0300

    banner created

---
+++ src/components/BannerComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="q-pa-md q-gutter-sm">
+    <q-banner rounded :class="$q.dark.isActive ? 'bg-grey-9' : 'bg-grey-2'">
+      <nav>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </nav>
+      <template v-slot:avatar>
+        <div style="display: flex; flex-direction: column; align-items: center;">
+          <img
+              src="https://cdn.quasar.dev/img/mountains.jpg"
+              style="width: 100px; height: 64px"
+          >
+          <slot name="avatarText"></slot>
+        </div>
+      </template>
+
+      {{ bannerText }}
+
+      <template v-slot:action>
+        <q-btn flat label="Retry" />
+      </template>
+    </q-banner>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BannerComponent',
+  props: {
+    bannerText: String
+  }
+}
+</script>
--- src/components/IndexPage.vue
+++ src/components/IndexPage.vue
@@ -1,17 +1,22 @@
 <template>
   <div>
-      <ButtonComponent />
+      <BannerComponent :bannerText="'A Biblia Sagrada Edição com quasar e vue'">
+        <template v-slot:avatarText>
+          <br>
+          <div>A Palavra de Deus</div>
+        </template>
+      </BannerComponent>
     <h1>testando</h1>
   </div>
 </template>
 
 <script>
-import ButtonComponent from '@/components/ButtonComponent.vue'
+import BannerComponent from '@/components/BannerComponent.vue'
 
 export default {
   name: 'IndexPage',
   components: {
-    ButtonComponent
+    BannerComponent
   }
 }
 </script>
